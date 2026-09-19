d76c109340c0b32f538a6170b53720da37b79eba
pitchy3 <mike@thevallones.com>
2026-09-19 12:16:14 -0700

    Fix validation and docs in tests/test_paths.py

--- tests/test_paths.py
+++ tests/test_paths.py
@@ -9,4 +9,15 @@ def test_longest_path_mapping_wins():
             PathMapping("plex", "/media/movies", "/data/_Movies"),
         )
     )
-    assert str(mapper.to_local("plex", "/media/movies/A/movie.mkv")) == "/data/_Movies/A/movie.mkv"
+    assert (
+        str(mapper.to_local("plex", "/media/movies/A/movie.mkv"))
+        == "/data/_Movies/A/movie.mkv"
+    )
+
+
+def test_path_mapping_can_translate_back_to_service_path():
+    mapper = PathMapper((PathMapping("radarr", "/movies", "/data/_Movies"),))
+    assert (
+        str(mapper.to_remote("radarr", "/data/_Movies/The Matrix/movie.mkv"))
+        == "/movies/The Matrix/movie.mkv"
+    )
